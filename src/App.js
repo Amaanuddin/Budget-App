201084b07c54f83d9153e4e94761d14c026f3147
Amaanuddin <amaanuddinparvez@gmail.com>
2018-12-24 08:36:34 +0530

    updated app.js users/Amaan

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React for Free
+            Learn React Online for Free
           </a>
         </header>
       </div>
